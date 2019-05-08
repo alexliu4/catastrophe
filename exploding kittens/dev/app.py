911f4e9feed156caa7f6575776130c8b3e5cc375
alexliu4 <aliu9@stuy.edu>
2019-05-08 12:31:24 -0400

    working on the account creation

---
+++ exploding kittens/dev/app.py
@@ -0,0 +1,108 @@
+from flask import Flask, render_template, session, request, url_for, redirect, flash
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route('/login')
+def login():
+    if 'user' in session:
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/auth', methods = ["POST"])
+def auth():
+    if 'user' in session:
+        return redirect(url_for('home'))
+    '''Intermediate to authenticate login by user'''
+    # # # Authenticate
+    username_input = request.form.get("username")
+    password_input = request.form.get("password")
+    all_usernames = db.get_all_users()
+    if username_input in all_usernames:
+        # If the hashes match
+        if md5_crypt.verify(password_input, all_usernames[username_input]):
+            # Log them in
+            session['user'] = username_input
+            return redirect(url_for("home"))
+        # Failed password and username match
+        else:
+            flash("Invalid password")
+    else:
+        # Username doesnt exist
+        flash("Invalid username")
+    return redirect(url_for("login"))
+
+@app.route('/register', methods = ["GET", "POST"])
+def register():
+    if 'user' in session:
+        return redirect(url_for('home'))
+    '''Adding users to the database'''
+    if request.form.get("reg_username") != None:
+        r_username = request.form.get("reg_username")
+        r_password = request.form.get("reg_password")
+        check_pass = request.form.get("check_password")
+        r_question = request.form.get("reg_question")
+        r_answer = request.form.get("reg_answer")
+        if r_username in db.get_all_users():
+            flash("Username taken")
+        elif r_password != check_pass:
+            flash("Passwords do not match!")
+        elif r_password.count(' ') != 0:
+            flash("Password can not contain spaces")
+        elif not r_username.isalnum():
+            flash("Username should be alphanumeric")
+        else:
+            if request.form.get("reg_question") != None:
+                session['user'] = r_username
+                db.add_userFull(r_username, md5_crypt.encrypt(r_password), r_question, md5_crypt.encrypt(r_answer))
+                return redirect(url_for("home"))
+            else:
+                session['user'] = r_username
+                db.add_user(r_username, md5_crypt.encrypt(r_password))
+                return redirect(url_for("home"))
+    return render_template('register.html')
+
+app.route('/reset', methods = ["GET", "POST"])
+def reset():
+    if 'user' in session:
+        return redirect(url_for('home'))
+    '''To reset userpassword'''
+    if request.form.get("reg_username") != None:
+        r_username = request.form.get("reg_username")
+        r_answer = request.form.get("reg_answer")
+        r_password = request.form.get("reg_password")
+        check_pass = request.form.get("check_password")
+        all_usernames = db.qaDict() #Returns dict {user:answer_to_question}
+        if r_username not in db.get_all_users():
+            flash("Username not found")
+        elif r_password != check_pass:
+            flash("Passwords do not match!")
+        elif r_password.count(' ') != 0:
+            flash("Password can not contain spaces")
+        elif not r_username.isalnum():
+            flash("Username should be alphanumeric")
+        else:
+            session['user'] = r_username
+            # checks the question and answer in the db
+            if r_username in all_usernames:
+                # if the hashes match
+                if md5_crypt.verify(r_answer, all_usernames[r_username]):
+                    # changes the user password
+                    db.update_pass(r_username, md5_crypt.encrypt(r_password))
+                    return redirect(url_for('home'))
+                else:
+                    flash("Error occurred")
+    return render_template('reset.html')
+
+@app.route('/logout', methods = ['GET'])
+def logout():
+    if 'user' in session:
+        session.pop('user')
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
