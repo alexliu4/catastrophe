24123d51d73c336f7f3a8564066db1ec73c34c4c
Karen Li <kli19@stuy.edu>
2019-05-12 19:08:27 -0400

    card lifts when clicked

--- exploding_kittens/dev/static/js/game.js
+++ exploding_kittens/dev/static/js/game.js
@@ -1,12 +1,32 @@
 //get canvas
 var c = document.getElementById("play_area");
 
-//instantiate conext of canvas
-var ctx = c.getContext("2d");
+var card = document.createElementNS("http://www.w3.org/2000/svg", "image");
+card.setAttributeNS("http://www.w3.org/1999/xlink","xlink:href", "./static/images/placeholder_card.png");
+card.setAttribute("width",200);
+card.setAttribute("height",200);
+card.setAttribute("x", 100);
+card.setAttribute("y", 400);
 
-var card = new Image();
-card.src = "static/images/placeholder_card.png";
+var requestID = 0;
 
-console.log("hi");
-ctx.drawImage(card, 100,100,100,100);
-console.log("hi");
+var shift_up = function(e){
+    var current = 0;
+    var shift = function(){
+	c.removeChild(e.target);
+	prev = Number(card.getAttribute("y"));
+	card.setAttribute("y", prev-1);
+	c.appendChild(card);
+	//cancel before animating in case  clicked multiple times
+	window.cancelAnimationFrame(requestID)
+	requestID = window.requestAnimationFrame(shift);
+	current += 2;
+	if (current > 50){
+	    window.cancelAnimationFrame(requestID)
+	};
+    }
+    shift();
+};
+
+card.addEventListener("click", shift_up);
+c.appendChild(card);
